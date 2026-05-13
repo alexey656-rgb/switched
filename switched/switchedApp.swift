e1601e9413eaa74908ab74c9f61f8024526b83a7
alexey656-rgb <alexey_656@icloud.com>
2026-05-13 23:43:42 +0300

    Initial Commit

---
+++ switched/switchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  switchedApp.swift
+//  switched
+//
+//  Created by Alexey Ivanov on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct switchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
